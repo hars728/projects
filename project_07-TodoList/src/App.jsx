f28ae1126bacae42d90632413b39e54b04362b93
HARSH KUMAR GUPTA <133455584+hars728@users.noreply.github.com>
2024-05-31 09:16:06 +0530

    it is simple one after that we have to make component

--- project_07-TodoList/src/App.jsx
+++ project_07-TodoList/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <center className="tood-container">
+      <h1>TODO LIST</h1>
+      <div class="container text-center">
 
-    </>
+        <div class="row">
+          <div class="col-4"><input type="text" placeholder="Enter todo name" />
+          </div>
+          <div class="col-4"> <input type="date" /></div>
+          <div class="col-2"><button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">Buy Milk
+          </div>
+          <div class="col-4"> 01/02/2023</div>
+          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">Buy books
+          </div>
+          <div class="col-4"> 01/12/2024</div>
+          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    </center>
   )
 }
 
